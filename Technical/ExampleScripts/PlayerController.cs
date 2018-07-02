451670922307c317fb796a47f2ca237c72c968c5
Vicki <vicki.tingle@gmail.com>
2018-07-02 19:44:33 +0100

    Movement prototype script.

---
+++ Technical/ExampleScripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    /// </summary>
+    public Vector2 speed = new Vector2(30, 30);
+
+    // 2 - Store the movement
+    private Vector2 movement;
+
+    private Rigidbody2D rbody;
+
+    // Use this for initialization
+    void Start()
+    {
+        Physics2D.gravity = Vector2.zero;
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        // 4 - Movement per direction
+        movement = new Vector2(
+          speed.x * inputX,
+          speed.y * inputY);
+    }
+
+        // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rbody == null)
+        {
+            rbody = GetComponent<Rigidbody2D>();
+        }
+
+        rbody.velocity = movement;
+    }
+}
